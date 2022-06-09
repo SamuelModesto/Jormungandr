26507477da946d0445fa81b953b828cd55084617
Samuel Modesto <samuelmodestoes@gmail.com>
2022-06-09 10:27:45 -0300

    project 0

---
+++ bands_name_generator_0/main.py
@@ -0,0 +1,4 @@
+print('Bem vindo ao gerador de nomes')
+city = input('Digite a cidade em que nasceu: \n')
+pet = input('Digite o nome do seu pet: \n')
+print('o nome da sua banda pode ser:', city, pet)
