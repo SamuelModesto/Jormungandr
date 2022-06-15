edd03eed059904a2f0ac1dcfef61fcaf9fef5269
Samuel Modesto <samuelmodestoes@gmail.com>
2022-06-14 21:29:41 -0300

    inserindo imagens no minigame

--- treasure_island/main.py
+++ treasure_island/main.py
@@ -6,28 +6,26 @@ def slowprint(texto, atraso=0.01):
         print(c, end='', flush=True)
         time.sleep(atraso)
 
-print('''
-*******************************************************************************
-          |                   |                  |                     |
- _________|________________.=""_;=.______________|_____________________|_______
-|                   |  ,-"_,=""     `"=.|                  |
-|___________________|__"=._o`"-._        `"=.______________|___________________
-          |                `"=._o`"=._      _`"=._                     |
- _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
-|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
-|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
-          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
- _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
-|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
-|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
-____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
-/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
-____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
-/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
-____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
-/______/______/______/______/______/______/______/______/______/______/____/___
-*******************************************************************************
-''')
+
+
+print('''              ,.  _~-.,                .
+           ~'`_ \/,_. \_
+          / ,"_>@`,__`~.)             |           .
+          | |  @@@@'  ",! .           .          '
+          |/   ^^@     .!  \          |         /
+          `' .^^^     ,'    '         |        .             .
+           .^^^   .          \                /          .
+          .^^^       '  .     \       |      /       . '
+.,.,.     ^^^             ` .   .,+~'`^`'~+,.     , '
+&&&&&&,  ,^^^^.  . ._ ..__ _  .'             '. '_ __ ____ __ _ .. .  .
+%%%%%%%%%^^^^^^%%&&;_,.-=~'`^`'~=-.,__,.-=~'`^`'~=-.,__,.-=~'`^`'~=-.,
+&&&&&%%%%%%%%%%%%%%%%%%&&;,.-=~'`^`'~=-.,__,.-=~'`^`'~=-.,__,.-=~'`^`'~=
+%%%%%&&&&&&&&&&&%%%%&&&_,.;^`'~=-.,__,.-=~'`^`'~=-.,__,.-=~'`^`'~=-.,__,
+%%%%%%%%%&&&&&&&&&-=~'`^`'~=-.,__,.-=~'`^`'~=-.,__,.-==--^'~=-.,__,.-=~'
+##mjy#####*"'
+_,.-=~'`^`'~=-.,__,.-=~'`^`'~=-.,__,.-=~'`^`'~=-.,.-=~'`^`'~=-.,__,.-=~'
+
+~`'^`'~=-.,__,.-=~'`^`'~=-.,__,.-=~'`^`'~=-.,__,.-=~'`^`'~=-.,__,.-=~'`^''')
 
 slowprint(
     'VOCÊ ACORDA EM UMA PRAIA DESERTA, A DOR NA NUCA É IRRITANTE E FORTE, COMO SE ALGUÉM TIVESSE BATIDO NELA POUCAS'
@@ -49,48 +47,142 @@ slowprint(
 
 choice1 = input(slowprint('AO CAMINHAR DENTRO DA CAVERNA VOCÊ CHEGA EM UMA BIFURCAÇÃO, ONDE VOCÊ QUER IR? ESQUERDA OU DIREITA?')).lower()
 if choice1 == 'esquerda':
-    print('VOCÊ CAMINHA POR ALGUNS MINUTOS, E LOGO ENCONTRA UMA AREA VASTA DEBAIXO DA MONTANHA, "UM LAGO DENTRO DE UMA CAVERNA!", '
-          'O PENSAMETO ESCAPA DE DENTRO DE VOCÊ. AO APROXIMAR A TOCHA DA ÁGUA VOCÊ PERCEBE QUE A AGUA É CRISTALINA E '
-          'É POSSÍVEL VER ALGUMAS MOEDAS DE PRATA DENTRO DA ÁGUA, PORÉM É FUNDO O SUFICIENTE PARA QUE NÃO ALCANCE '
-          'OS PÉS NO CHÃO E PASSE ANDANDO. VOCÊ NOTA SINAIS DE EMBARCAÇÃO, E UM CAMINHO AO FUNDO, COMO SE PASSASSEM CANOAS POR ALI.')
-    choice2 = input('O QUE VOCÊ QUER FAZER? NADAR ATÉ O OUTRO LADO OU ESPERAR POR UMA CANOA? \n').lower()
+    print(''' 
+     **********************************************************************************************                                 
+       ~~~~ ~ ~======~~  ~~ ~ ~~~~ ~ ~======~~  ~~ ~ ~~~~ ~ ~======~~  ~~ ~ ~~~~ ~ ~======~~  ~~ ~
+       ~~~~ ~ ~======~~  ~~ ~ ~~~~ ~ ~======~~  ~~ ~ ~~~~ ~ ~======~~  ~~ ~ ~~~~ ~ ~======~~  ~~ ~
+        ~~~~ ~ ~======~~  ~~ ~ ~~~~ ~ ~======~~  ~~ ~ ~~~~ ~ ~======~~  ~~ ~ ~~~~ ~ ~======~~  ~~ ~
+        ~~~~ ~ ~======~~  ~~ ~ ~~~~ ~ ~======~~  ~~ ~ ~~~~ ~ ~======~~  ~~ ~ ~~~~ ~ ~======~~  ~~ ~
+        ~~~~ ~ ~======~~  ~~ ~ ~~~~~~ ~~~~~  ~~~ ~ ~~~~ ~ ~======~~  ~~ ~ ~~~~ ~ ~======~~  ~~ ~
+        ~~~~ ~ ~======~~  ~~ ~ ~~~~ ~ ~======~~  ~~ ~ ~~~~ ~ ~======~~  ~~ ~ ~~~~ ~ ~======~~  ~~ ~
+       ~~~~ ~ ~======~~  ~~ ~ ~~~~ ~ ~======~~  ~~ ~ ~~~~ ~ ~======~~  ~~ ~ ~~~~ ~ ~======~~  ~~ ~
+        ~~~~ ~ ~======~~  ~~ ~ ~~~~ ~ ~======~~  ~~ ~ ~~~~ ~ ~======~~  ~~ ~ ~~~~ ~ ~======~~  ~~ ~
+        ~~~~ ~ ~======~~  ~~ ~ ~~~~ ~ ~======~~  ~~ ~ ~~~~ ~ ~======~~  ~~ ~ ~~~~ ~ ~======~~  ~~ ~
+        ~~~~ ~ ~======~~  ~~ ~ ~~~~~~ ~~~~~  ~~~ ~ ~~~~ ~ ~======~~  ~~ ~ ~~~~ ~ ~======~~  ~~ ~ 
+    ***********************************************************************************************   
+        ''')
+    slowprint(
+        'VOCÊ CAMINHA POR ALGUNS MINUTOS, E LOGO ENCONTRA UMA AREA VASTA DEBAIXO DA MONTANHA, "UM LAGO DENTRO DE UMA CAVERNA!", '
+        'O PENSAMETO ESCAPA DE DENTRO DE VOCÊ. AO APROXIMAR A TOCHA DA ÁGUA VOCÊ PERCEBE QUE A AGUA É CRISTALINA E '
+        'É POSSÍVEL VER ALGUMAS MOEDAS DE PRATA DENTRO DA ÁGUA, PORÉM É FUNDO O SUFICIENTE PARA QUE NÃO ALCANCE '
+        'OS PÉS NO CHÃO E PASSE ANDANDO. VOCÊ NOTA SINAIS DE EMBARCAÇÃO, E UM CAMINHO AO FUNDO, COMO SE PASSASSEM CANOAS POR ALI.')
+    choice2 = input(slowprint('O QUE VOCÊ QUER FAZER? NADAR ATÉ O OUTRO LADO OU ESPERAR POR UMA CANOA? \n')).lower()
     if choice2 == 'esperar':
-        print('VOCE ESPERA, E DEPOIS DE ALGUM TEMPO, UMA CANOA APARECE AO LONGE DO LAGO, UMA PEQUENA LAMPARINA NA FRENTE'
-              'DA CANOA E ALGUEM EM PÉ COM UM GRANDE BASTÃO GUIANDO A EMBARCAÇÃO. QUANDO A CANOA SE APROXIMA VOCE ACENA'
-              'E BALANÇA A TOCHA COMO UM SINAL DE QUE PRECISA DE UMA CARONA ATÉ O OUTRO LADO. A EMBARCAÇÃO VIRA EM SUA DIREÇÃO'
-              'E SEGUE ATÉ QUE VOCÊ CONSIGA ALCANÇA-LA, O SENHOR GENTILMENTE LHE OFERECE CARONA POR ALGUMAS MOEDAS DE PRATA'
-              'POREM VOCE NAO AS TEM. MAS PROMETE DIVIDIR O TESOURO CASO O ENCONTRE, O SENHOR DECIDE AJUDAR E VOCE EMBARCA.')
-        print('APOS ALGUM TEMPO NAVEGANDO, VOCE AVISTA UM LOCAL DE SUBIDA ONDE APARENTEMENTE MUITOS TENTARAM SUBIR, POIS EXISTEM'
-              'ESQUELETOS NA PARTE DE BAIXO DA SUBIDA, VOCE DECIDE QUE QUER VER O QUE HÁ NESTE LOCAL E PEDE PARA QUE O SENHOR'
-              'GUIE A CANOA O MAIS PROXIMO POSSIVEL. AO ENCOSTAR VOCE SALTA E CONSEGUE SE AGARRAR A PARTE SUPERIOR, A ESCALADA É'
-              'DIFÍCIL MAS VOCÊ CONSEGUE, E PERCEBE QUE EXISTE UM TÚNEL ILUMINADO POR RAIOS DE SOL QUE CONSEGUE TRANPASSAR POR ALGUMAS FRESTAS.'
-              'AO FINAL DO TÚNEL EXISTEM 3 PORTAS DE TRES CORES DIFERENTES E VOCE PRECISA ESCOLHER UMA: ')
+        print('''
+                                         __________/\__________
+             ~~~~~~ ~======~~  ~~~~~~ ~~~\____________________/~~~~~~ ~======~~  ~~ ~ 
+             ~~~~~ ~======~~  ~~~~~ ~ ~~~ ~~~. ~  ~~~~~~ ~======~~  ~~ ~ ~~~~ ~ ~=====
+        *******************************************************************************
+        ''')
+        slowprint(
+            'VOCE ESPERA, E DEPOIS DE ALGUM TEMPO, UMA CANOA APARECE AO LONGE DO LAGO, UMA PEQUENA LAMPARINA NA FRENTE'
+            'DA CANOA E ALGUEM EM PÉ COM UM GRANDE BASTÃO GUIANDO A EMBARCAÇÃO. QUANDO A CANOA SE APROXIMA VOCE ACENA'
+            'E BALANÇA A TOCHA COMO UM SINAL DE QUE PRECISA DE UMA CARONA ATÉ O OUTRO LADO. A EMBARCAÇÃO VIRA EM SUA DIREÇÃO'
+            'E SEGUE ATÉ QUE VOCÊ CONSIGA ALCANÇA-LA, O SENHOR GENTILMENTE LHE OFERECE CARONA POR ALGUMAS MOEDAS DE PRATA'
+            'POREM VOCE NAO AS TEM. MAS PROMETE DIVIDIR O TESOURO CASO O ENCONTRE, O SENHOR DECIDE AJUDAR E VOCE EMBARCA.'
+            'APOS ALGUM TEMPO NAVEGANDO, VOCE AVISTA UM LOCAL DE SUBIDA ONDE APARENTEMENTE MUITOS TENTARAM SUBIR, POIS EXISTEM'
+            'ESQUELETOS NA PARTE DE BAIXO DA SUBIDA, VOCE DECIDE QUE QUER VER O QUE HÁ NESTE LOCAL E PEDE PARA QUE O SENHOR'
+            'GUIE A CANOA O MAIS PROXIMO POSSIVEL. AO ENCOSTAR VOCE SALTA E CONSEGUE SE AGARRAR A PARTE SUPERIOR, A ESCALADA É'
+            'DIFÍCIL MAS VOCÊ CONSEGUE, E PERCEBE QUE EXISTE UM TÚNEL ILUMINADO POR RAIOS DE SOL QUE CONSEGUE TRANPASSAR POR ALGUMAS FRESTAS.'
+            'AO FINAL DO TÚNEL EXISTEM 3 PORTAS DE TRES CORES DIFERENTES E VOCE PRECISA ESCOLHER UMA: ')
         choice3 = input('QUAL PORTA VOCÊ QUER ABRIR? VERMELHO, AZUL ou AMARELO?\n').lower()
         if choice3 == 'amarelo':
-            print('PARABENS VOCE ENCONTROU O TESOURO PERDIDO, PAGOU UMA PARTE AO SENHOR E VOLTOU AO CAPITÃO JHONY SPADER E SALVOU A SUA AMADA!')
+            print('''
+            *******************************************************************************
+                      |                   |                  |                     |
+             _________|________________.=""_;=.______________|_____________________|_______
+            |                   |  ,-"_,=""     `"=.|                  |
+            |___________________|__"=._o`"-._        `"=.______________|___________________
+                      |                `"=._o`"=._      _`"=._                     |
+             _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+            |                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+            |___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+                      |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+             _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+            |                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+            |___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+            ____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+            /______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+            ____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+            /______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+            ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+            /______/______/______/______/______/______/______/______/______/______/____/___
+            *******************************************************************************
+            ''')
+            slowprint('PARABENS VOCE ENCONTROU O TESOURO PERDIDO, PAGOU UMA PARTE AO SENHOR E VOLTOU AO CAPITÃO JHONY SPADER E SALVOU A SUA AMADA!')
         elif choice3 == 'vermelho':
-            print('game over')
+            print('''
+         *******************************************************************************   
+                                               ____                      ,
+                                  /---.'.__             ____//
+                                       '--.\           /.---'
+                                  _______  \\         //
+                                /.------.\  \|      .'/  ______
+                               //  ___  \ \ ||/|\  //  _/_----.\__
+                              |/  /.-.\  \ \:|< >|// _/.'..\   '--'
+                                 //   \'. | \'.|.'/ /_/ /  \\
+                                //     \ \_\/" ' ~\-'.-'    \\
+                               //       '-._| :H: |'-.__     \\
+                              //           (/'==='\)'-._\     ||
+                              ||                        \\    \|
+                              ||                         \\    '
+                              |/                          \\
+                                                           ||
+                                                           ||
+                                                           \\
+         *******************************************************************************      
+            ''')
         else:
-            print('game over')
+            print('''
+        ******************************************************************************   
+                                
+                                   (  .      )
+                               )           (              )
+                                     .  '   .   '  .  '  .
+                            (    , )       (.   )  (   ',    )
+                             .' ) ( . )    ,  ( ,     )   ( .
+                          ). , ( .   (  ) ( , ')  .' (  ,    )
+                         (_,) . ), ) _) _,')  (, ) '. )  ,. (' )
+                        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+        ******************************************************************************   
+            ''')
+            slowprint('VOCE ABRE A PORTA E CHAMAS TE QUEIMAM, SEM DEIXAR NENHUMA CHANCE PARA SOBREVIVENCIA GAME-OVER!')
     else:
-        print('VOCÊ TENTA NADAR MAS NÃO CONTAVA QUE ESTE LAGO ESTARIA INFESTADO DE CROCODILOS, '
-              'OS ANIMAIS TE ABOCANHAM ARRANCANDO OS SEUS MEMBROS MANCHANDO A ÁGUA CRISTALINA DO LAGO COM O SEU '
-              'GAME-OVER!')
         print(''' 
-*******************************************************************************
-                            _.---._     .---.
-            __...---' .---. `---'-.   `.
-  ~ -~ -.-''__.--' _.'( | )`.  `.  `._ :
- -.~~ .'__-'_ .--'' ._`---'_.-.  `.   `-`.
-  ~ ~_~-~-~_ ~ -._ -._``---. -.    `-._   `.
-    ~- ~ ~ -_ -~ ~ -.._ _ _ _ ..-_ `.  `-._``--.._
-     ~~-~ ~-_ _~ ~-~ ~ -~ _~~_-~ -._  `-.  -. `-._``--.._.--''. ~ -~_
-         ~~ -~_-~ _~- _~~ _~-_~ ~-_~~ ~-.___    -._  `-.__   `. `. ~ -_~
-             ~~ _~- ~~- -_~  ~- ~ - _~~- _~~ ~---...__ _    ._ .` `. ~-_~
-                ~ ~- _~~- _-_~ ~-_ ~-~ ~_-~ _~- ~_~-_~  ~--.....--~ -~_ ~
-                     ~ ~ - ~  ~ ~~ - ~~-  ~~- ~-  ~ -~ ~ ~ -~~-  ~- ~-~
-*******************************************************************************
-                     ''')
-else:
-    print('game over')
+        *******************************************************************************
+                                    _.---._     .---.
+                    __...---' .---. `---'-.   `.
+          ~ -~ -.-''__.--' _.'( | )`.  `.  `._ :
+         -.~~ .'__-'_ .--'' ._`---'_.-.  `.   `-`.
+          ~ ~_~-~-~_ ~ -._ -._``---. -.    `-._   `.
+            ~- ~ ~ -_ -~ ~ -.._ _ _ _ ..-_ `.  `-._``--.._
+             ~~-~ ~-_ _~ ~-~ ~ -~ _~~_-~ -._  `-.  -. `-._``--.._.--''. ~ -~_
+                 ~~ -~_-~ _~- _~~ _~-_~ ~-_~~ ~-.___    -._  `-.__   `. `. ~ -_~
+                     ~~ _~- ~~- -_~  ~- ~ - _~~- _~~ ~---...__ _    ._ .` `. ~-_~
+                        ~ ~- _~~- _-_~ ~-_ ~-~ ~_-~ _~- ~_~-_~  ~--.....--~ -~_ ~
+                             ~ ~ - ~  ~ ~~ - ~~-  ~~- ~-  ~ -~ ~ ~ -~~-  ~- ~-~
+        *******************************************************************************
+                             ''')
+        slowprint('VOCÊ TENTA NADAR MAS NÃO CONTAVA QUE ESTE LAGO ESTARIA INFESTADO DE CROCODILOS, '
+                  'OS ANIMAIS TE ABOCANHAM ARRANCANDO OS SEUS MEMBROS MANCHANDO A ÁGUA CRISTALINA DO LAGO COM O SEU '
+                  'GAME-OVER!')
 
+else:
+    print('''
+     *******************************************************************************      
+                         ________________________________         
+                          /                                "-_          
+                         /      .  |  .                       \          
+                        /      : \ | / :                       \         
+                       /        '-___-'                         \      
+                      /_________________________________________ \      
+                           _______| |________________________--""-L 
+                          /       F J                              \ 
+                         /       F   J                              L
+                        /      :'     ':                            F
+                       /        '-___-'                            / 
+                      /_________________________________________--"  
+   ***********************************************************************************
+ ''')
+    slowprint('VOCE CAMINHA E MESMO COM A TOCHA NAO PERCEBE UM BURACO NA SUA FRENTE, ESCORREGA E CAI EM UM BURACO PROFUNDO '
+              'SEM CHANCE DE ESCAPATÓRIA OU SOBREVIVÊNCIA GAME-OVER!')
